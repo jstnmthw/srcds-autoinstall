6310f4a532e9cf9f19c7cc0c9331f9c29b0a43b6
Justin <web@jstn.ly>
2024-10-06 15:38:01 +0700

    Refactor: Update s3-download.sh script to improve file path handling

--- scripts/utils/s3-download.sh
+++ scripts/utils/s3-download.sh
@@ -5,33 +5,10 @@ S3_BUCKET="archive-6"
 S3_FILE_PATH="gameservers"
 S3_FILE="cs-configs.zip"
 
-# Define an array with the names of the required environment variables
-# required_env_vars=(
-#     "S3_BUCKET"
-#     "S3_FILE_PATH"
-#     "S3_FILE"
-#     "AWS_REGION"
-#     "AWS_ACCESS_KEY_ID"
-#     "AWS_SECRET_ACCESS_KEY"
-# )
-
-# Check if the required environment variables are set
-for var_name in "${required_env_vars[@]}"; do
-    if [[ -z "${!var_name}" ]]; then
-        echo "Error: $var_name environment variable is not set. Please check your .env file."
-        exit 1
-    fi
-done
-
 echo "Downloading config files from S3..."
 
-# Set AWS credentials
-# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-# aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-# aws configure set default.region $AWS_REGION
-
 # Use AWS CLI command to download the file from S3
-if ! aws s3 cp s3://${S3_BUCKET}/${S3_FILE_PATH}/${S3_FILE} /tmp; then
+if ! aws s3 cp s3://$S3_BUCKET/$S3_FILE_PATH/$S3_FILE /tmp/$S3_FILE; then
     echo "Error: Failed to download file from S3. Please check your S3 bucket and file path."
     exit 1
 fi
